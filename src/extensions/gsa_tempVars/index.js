078fa4ab0d5a5bf2f815dc0e7737d5bcbf152d7d
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2024-02-01 20:45:52 -0700

    allow drop anywhere on temp var

--- src/extensions/gsa_tempVars/index.js
+++ src/extensions/gsa_tempVars/index.js
@@ -72,6 +72,7 @@ class tempVars {
                             defaultValue: 'Variable'
                         }
                     },
+                    allowDropAnywhere: true,
                     blockType: BlockType.REPORTER
                 },
                 '---',
