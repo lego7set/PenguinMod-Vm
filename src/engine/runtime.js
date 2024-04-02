3b27f489c3c9cb3f7d3248412d68ac9f277f2ff6
Aness6040 <aness2805@gmail.com>
2024-04-02 20:22:29 +0200

    OOO

--- src/engine/runtime.js
+++ src/engine/runtime.js
@@ -53,7 +53,6 @@ const defaultBlockPackages = {
     scratch3_sensing: require('../blocks/scratch3_sensing'),
     scratch3_data: require('../blocks/scratch3_data'),
     scratch3_procedures: require('../blocks/scratch3_procedures'),
-    pm_liveTests: require('../blocks/pm_live tests')
 };
 
 const interpolate = require('./tw-interpolate');
