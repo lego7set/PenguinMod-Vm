03ccbb996a73d9c1d6f864c56b22932afde9be5e
godslayerakp <74981904+RedMan13@users.noreply.github.com>
2024-05-03 22:24:42 -0700

    use `in` instead of truthy

--- src/engine/runtime.js
+++ src/engine/runtime.js
@@ -1672,7 +1672,7 @@ class Runtime extends EventEmitter {
                 blockJSON.checkboxInFlyout = true;
             }
         }
-        if (blockInfo.blockType === BlockType.LOOP || (blockInfo.branchIndicator ?? blockInfo.branchIconURI)) {
+        if (blockInfo.blockType === BlockType.LOOP || ('branchIndicator' in blockInfo || 'branchIconURI' in blockInfo)) {
             // Add icon to the bottom right of a loop block
             blockJSON[`lastDummyAlign${outLineNum}`] = 'RIGHT';
             blockJSON[`message${outLineNum}`] = '%1';
